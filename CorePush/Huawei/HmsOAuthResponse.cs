a92b0aa37ae7bcd17328371c1753363a224a4334
Bastian Noffer <bastian.noffer@real-digital-agency.de>
2020-03-03 11:59:53 +0100

    [TASK][APPBACKEND-6] -Implemented logic according to Huawei Docs for sending messages via HMS Core Push

---
+++ CorePush/Huawei/HmsOAuthResponse.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace CorePush.Huawei
+{
+    public class HmsOAuthResponse
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("expires_in")]
+        public int ExpiresIn { get; set; }
+
+        [JsonProperty("scope")]
+        public string Scope { get; set; }
+
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; }
+
+        [JsonProperty("error")]
+        public int Error { get; set; }
+
+        [JsonProperty("sub_error")]
+        public int SubError { get; set; }
+
+        [JsonProperty("error_description")]
+        public string ErrorDescription { get; set; }
+    }
+}
---
+++ CorePush/Huawei/HmsSendResponse.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+namespace CorePush.Huawei
+{
+    public class HmsSendResponse
+    {
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("msg")]
+        public string Message { get; set; }
+
+        [JsonProperty("requestId")]
+        public string RequestId { get; set; }
+    }
+}
