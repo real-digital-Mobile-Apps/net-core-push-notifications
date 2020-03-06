7de6759ac5ebc9e1c75870e007fe79191d702f45
Bastian Noffer <bastian.noffer@real-digital-agency.de>
2020-03-06 08:52:38 +0100

    [TASK][APPBACKEND-5] -added multipart send support

--- CorePush/Google/FcmSender.cs
+++ CorePush/Google/FcmSender.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -38,13 +39,47 @@ namespace CorePush.Google
             jsonObject.Remove("to");
             jsonObject.Add("to", JToken.FromObject(deviceId));
             var json = jsonObject.ToString();
-
+            
             using var httpRequest = new HttpRequestMessage(HttpMethod.Post, fcmUrl);
             httpRequest.Headers.Add("Authorization", $"key = {serverKey}");
             httpRequest.Headers.Add("Sender", $"id = {senderId}");
             httpRequest.Content = new StringContent(json, Encoding.UTF8, "application/json");
 
             using var response = await lazyHttp.Value.SendAsync(httpRequest);
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            return JsonHelper.Deserialize<FcmResponse>(responseString);
+        }
+
+        /// <summary>
+        /// Send firebase notification to multiple targets.
+        /// Please check out payload formats:
+        /// https://firebase.google.com/docs/cloud-messaging/concept-options#notifications
+        /// The SendMultipleAsync method will add/replace "to" value with deviceId
+        /// </summary>
+        /// <param name="multiplePayloads">key = deviceId, value = payload</param>
+        /// <exception cref="HttpRequestException">Throws exception when not successful</exception>
+        public async Task<FcmResponse> SendMultipleAsync(Dictionary<string, object> multiplePayloads)
+        {
+            var batchRequest = new HttpRequestMessage(HttpMethod.Post, fcmUrl);
+            var batchContent = new MultipartContent("mixed", "--subrequest_boundary");
+            batchRequest.Content = batchContent;
+
+            foreach (var data in multiplePayloads)
+            {
+                var jsonObject = JObject.FromObject(data.Value);
+                jsonObject.Remove("to");
+                jsonObject.Add("to", JToken.FromObject(data.Key));
+                var json = jsonObject.ToString();
+
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                content.Headers.Add("Authorization", $"key = {serverKey}");
+                content.Headers.Add("Sender", $"id = {senderId}");
+                batchContent.Add(content);
+            }
+
+            using var response = await lazyHttp.Value.SendAsync(batchRequest);
             response.EnsureSuccessStatusCode();
             var responseString = await response.Content.ReadAsStringAsync();
 
